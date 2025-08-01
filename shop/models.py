64e428889b3f0d5cbede62a799ede4ae4a90634b
Vladislav123-hue <6o4tqdmx@students.codeinstitute.net>
2025-08-01 15:23:21 +0300

    custom routes, view, create first html

--- shop/models.py
+++ shop/models.py
@@ -15,7 +15,7 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
-    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
