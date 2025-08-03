cb24c14723c3d78630c8a4befd1f07ab2d84c537
Vladislav123-hue <6o4tqdmx@students.codeinstitute.net>
2025-08-03 23:18:38 +0300

    create model, admin, app, views

---
+++ alumma/styles/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Style(models.Model):
+    name = models.CharField(max_length=25)
+    description = models.TextField(
+        blank=True,                    
+        null=False                     
+    )
